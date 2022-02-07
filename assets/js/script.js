c7e1cd3357f030378c81283690c05e8a8bf6caaf
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-02-06 21:28:18 -0500

    fetch was not part of a function, which caused errored - corrected

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,12 @@
 
-
-
 var currentDay = document.getElementById("currentDay"); //current time & day in header
-var apiKey = 'e4e829dc730191eee614fe35436601e0';
+var apiKey = '7312f736322322a27fef229205b4ab79';
 var searchForm = document.getElementById('search-box');
 var searchCity = document.getElementsByClassName("form-control")[0];
 var weatherToday = document.getElementById("today");
 var fiveDay = document.getElementById("fiveDayForecast");
 var historyForecast = document.getElementById("historyForecast");
 var searchHistory = [];
-
 
 
 
@@ -25,19 +22,32 @@ setInterval(displayTime,1000);
 
 
 //Search history function that will be added to left sidebar.
-function listHistoryForecast () {
-    historyForecast.innerHTML = '';
+function listHistoryForecast() {
+    historyForecast.innerHTML = x;
 }
 
-     
-var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + searchCity + ',&appid=' + apiKey;
+for (var i = searchHistory.length - 1; i >= 0; i--) {
+btn
+}
 
-// fetch (apiUrl)
-//     .then(function(response) {
-//         return response.json();
-//     }).then(function (data){
-//         renderItems(searchCity,data);
-//     })
+
+// fetch api URl then turn response into a json and show city and data 
+var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + ',&appid=' + apiKey;
+
+function fetchAPI () {
+
+fetch (apiUrl)
+    .then(function(res) {
+        return res.json();
+    }).then(function (data){
+        renderItems(city, data);
+    });
+    // .catch(function(err){
+    //     console.error("ERROR");
+    // });
+
+}
+
 
 
 
@@ -45,7 +55,7 @@ var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + searchCity +
 let cities = [];
 
 // this API website naming syntax that calls for a city
-// function getCityWeather(city, isClicked) {
+function getCityWeather(city, isClicked) {}
     
 
     //  Calling the API
@@ -55,10 +65,10 @@ let cities = [];
 
 
 // need to make a functional weather button
+var btn = document.querySelector('.btn')
 
 //For loop needed for 5-day forecast
 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
 //working on code for loop 
-// for (let i = 0; len=days.length; text = " ", i < len; i++){ });
-
+// for (let i = 0; len=days.length; text = " ", i < len; i++)
